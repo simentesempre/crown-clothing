9b4f8b3dd3eb21b099ac45d317e0b10398c3ffed
simentesempre <federico.portoghese@gmail.com>
2019-12-21 12:35:02 +0100

    Adds all data to redux

--- src/pages/shoppage/shoppage.component.jsx
+++ src/pages/shoppage/shoppage.component.jsx
@@ -1,28 +1,23 @@
 import React from 'react'
-import SHOP_DATA from './shoppage.data.js'
+import { connect } from 'react-redux'
 
 import PreviewCollection from '../../components/preview-collection/preview-collection.component.jsx'
 
-class ShopPage extends React.Component  {
-    constructor(props) {
-        super(props)
-        this.state = {
-            collections: SHOP_DATA
-        }
-    }
-    render() {
-        const { collections } = this.state
-        return (
-            <div className="shop-page">
-                {
-                    collections.map(({id, ...others}) => {
-                        return <PreviewCollection key={id} {...others} />
-                    })
-                }
-            </div>
-            
-        )
-    }
+const ShopPage = ({collections}) =>  {
+    return (
+        <div className="shop-page">
+            {
+                collections.map(({id, ...others}) => {
+                    return <PreviewCollection key={id} {...others} />
+                })
+            }
+        </div>
+        
+    )
 }
 
-export default ShopPage
+const mapStateToProps = ({ shop: { collections }}) => ({
+    collections
+})
+
+export default connect(mapStateToProps)(ShopPage)
