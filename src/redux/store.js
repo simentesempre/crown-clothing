1530842967da99e125f1651dd0bedc6ba07dd1dc
simentesempre <federico.portoghese@gmail.com>
2019-12-22 11:21:19 +0100

    Removes middlewares from production

--- src/redux/store.js
+++ src/redux/store.js
@@ -4,7 +4,7 @@ import { persistStore } from 'redux-persist'
 
 import rootReducer from './root.reducer'
 
-const middlewares = [logger]
+const middlewares = (process.env.NODE_ENV === 'development') ? [logger] : []
 
 export const store = createStore(rootReducer, applyMiddleware(...middlewares))
 
